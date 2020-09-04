277cf2d6d10a51b1af262e0aee9353cfbb9dc0fb
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-09-04 12:46:06 -0700

    refactored functions on solarYears.js and refactored tests on solarYear.test.js

--- _tests_/solarYear.test.js
+++ _tests_/solarYear.test.js
@@ -4,28 +4,28 @@ describe('SolarYears', () => {
   const solarYears = new SolarYears();
 
   test('should correctly return the solar years of mercury at 0.24 Earth Years', () => {
-    solarYears.mercuryYear(10);
-    expect(solarYears.mercury).toEqual(10/.24);
+    solarYears.solarYearEquation(10);
+    expect(solarYears.mercury).toEqual(41.6);
   });
 
   test('should correctly return the solar years of Venus at 0.62 Earth Years', () => {
-    solarYears.venusYear(10);
-    expect(solarYears.venus).toEqual(10/.62);
+    solarYears.solarYearEquation(10);
+    expect(solarYears.venus).toEqual(16.1);
   });
 
   test('should correctly return inputted Earth years', () => {
-    solarYears.earthYear(10);
+    solarYears.solarYearEquation(10);
     expect(solarYears.earth).toEqual(10);
   });
 
   test('should correctly return the solar years of Mars at 1.88 Earth Years', () => {
-    solarYears.marsYear(10);
-    expect(solarYears.mars).toEqual(10/1.88);
+    solarYears.solarYearEquation(10);
+    expect(solarYears.mars).toEqual(5.3);
   });
 
   test('should correctly return the solar years of Jupiter at 11.86 Earth Years', () => {
-    solarYears.jupiterYear(10);
-    expect(solarYears.jupiter).toEqual(10/11.86);
+    solarYears.solarYearEquation(10);
+    expect(solarYears.jupiter).toEqual(0.8);
   });
 
   test('should correctly return a random age between 72 and 97', () => {
@@ -34,9 +34,9 @@ describe('SolarYears', () => {
   });
 
   test('should correctly return the random age between 72 and 97 minus the inputted age', () => {
-    solarYears.solarYearsLeft(53);
-    expect(solarYears.yearsLeft <= 44 && solarYears.yearsLeft >= 19).toBe(true);
-    expect(solarYears.mercury <= 44 && solarYears.yearsLeft >= 19).toBe(true);
-    expect(solarYears.
+    solarYears.earthYearsLeft(53);
+    // expect(solarYears.yearsLeft <= 44 && solarYears.yearsLeft >= 19).toBe(true);
+    // expect(solarYears.mercury <= 44 && solarYears.yearsLeft >= 19).toBe(true);
+    expect(solarYears.yearsLeft).toBe(true);
   });
 });
--- src/js/solarYear.js
+++ src/js/solarYear.js
@@ -9,39 +9,47 @@ export default class SolarYears {
     this.yearsLeft = yearsLeft;
   }
 
-  mercuryYear (age) {
-    this.mercury = age / 0.24;
+  solarYearEquation (age) {
+    this.mercury = Math.floor(10* age / 0.24)/10;
+    this.venus = Math.floor(10 * age / 0.62)/10;
+    this.earth = age;
+    this.mars = Math.floor(10 * age / 1.88)/10;
+    this.jupiter = Math.floor(10* age / 11.86)/10;
   }
-  venusYear (age) {
-    this.venus = age / 0.62;
-  }
-
-  earthYear (age) {
-   this.earth = age;
-  }
-
-  marsYear (age) {
-    this.mars = age / 1.88;
-   }
-
-   jupiterYear (age) {
-    this.jupiter = age / 11.86;
-   }
-
+  
    randomNumber () {
     let random = (Math.random() * 25) + 72;
-    this.randomAge = Math.round(10 * random)/10;
+    this.randomAge = Math.floor(10 * random)/10;
 
    }
 
-   solarYearsLeft (age, mercuryLeft, venusLeft, marsLeft, jupiterLeft) {
-    this.yearsLeft = this.randomAge - age;
-    mercuryLeft = Math.round(10 * (this.yearsLeft / 0.24) / 10);
-    venusLeft = Math.round(10 * (this.yearsLeft / 0.62) / 10);
-    marsLeft = Math.round(10 * (this.yearsLeft / 1.88) / 10);
-    jupiterLeft = jupiterYear(this.yearsLeft);
+   solarYearsLeft (age) {
+     this.yearsLeft = [];
+     this.yearsLeft.push(this.randomAge - age);
+    //  this.earthLeft = this.yearsLeft[0];
+    //  this.yearsLeft.push(this.earthLeft/0.24);
+    //  this.yearsLeft.push(this.earthLeft/0.62);
+    //  this.yearsLeft.push(this.earthLeft/1.88);
+    //  this.yearsLeft.push(this.earthLeft/11.86);
+     this.yearsLeft = this.yearsLeft.map(function(element) {
+       return Number(element.toFixed(1));
+     });
+   }
+
+  //  solarYearsLeft (age) {
     
-   }
+    // this.earthLeft = this.yearsLeft[0];
+    // this.yearsLeft.push(math.round(10 * (this.earthLeft / 0.24))/10);
+    // this.yearsLeft.push(math.round(10 * (this.earthLeft / 0.62))/10);
+    // this.yearsLeft.push(math.round(10 * (this.earthLeft / 1.88))/10);
+    // this.yearsLeft.push(math.round(10 * (this.earthLeft / 11.86))/10);
+    // this.yearsLeft = this.randomAge - age;
+    // mercuryLeft = Math.round(10 * (this.yearsLeft / 0.24) / 10);
+    // venusLeft = Math.round(10 * (this.yearsLeft / 0.62) / 10);
+    // marsLeft = Math.round(10 * (this.yearsLeft / 1.88) / 10);
+    // jupiterLeft = jupiterYear(this.yearsLeft);
+    
+  //  }
 }
 
 
