e934722092aa94fec3545de6146d0d763d3672bf
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-09-04 09:50:12 -0700

    Passed test on first spec for Mercury solar years.

--- _tests_/solarYear.test.js
+++ _tests_/solarYear.test.js
@@ -5,6 +5,6 @@ describe('SolarYears', () => {
   test('should correctly return the solar years of mercury at 0.24 Earth Years', () => {
     const solarYears = new SolarYears();
     solarYears.mercuryYear(10);
-    expect(solarYears.mercury).toEqual(41.5);
+    expect(solarYears.mercury).toEqual(10/.24);
   });
 });
